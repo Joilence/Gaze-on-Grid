a4d8a30d10293ca95a89f369667a54aadb73fb6f
Joilence <cjyk84719@gmail.com>
2020-08-25 18:16:11 +0200

    fix: drop rows by index

--- data-analysis/preprocessing.py
+++ data-analysis/preprocessing.py
@@ -12,4 +12,4 @@ def get_participant_names(path):
 def remove_outlier_by_z(data, attr, z_th=3):
     zs = stats.zscore(data[attr])
     outlier = [i for i, z in enumerate(zs) if abs(z) >= 3]
-    return data.drop(outlier)
+    return data.drop(data.index[outlier])
